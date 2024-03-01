6f6cfff87cb81933e361282f4c59bc311d383458
LuongBaHung <157610997+LuongBaHung@users.noreply.github.com>
2024-03-01 09:52:33 +0700

    Commit bai tap 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+namespace buoi3_BTVN1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Nhap thoi gian 12h (hh:mm:ss tt): ");
+                    string inputTime12H = Console.ReadLine();
+                    string outputTime24H = Convert12HTo24H(inputTime12H);
+                    Console.WriteLine("Thoi gian 24h la: " + outputTime24H);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Khong dung dinh dang");
+                }
+            }
+        }
+
+        static string Convert12HTo24H(string time12H)
+        {
+            DateTime time12HFormat = DateTime.ParseExact(time12H, "hh:mm:ss tt", System.Globalization.CultureInfo.InvariantCulture);
+            string time24HFormat = time12HFormat.ToString("HH:mm:ss");
+
+            return time24HFormat;
+
+        }
+    }
+}
